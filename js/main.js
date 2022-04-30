7bc38d5a4dcda37e541ba57b62b0c483d1a28294
adylansd28 <97264058+adylansd28@users.noreply.github.com>
2022-04-30 17:11:01 -0500

    W02 Starting

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let taskList = document.querySelector(".task-list");
+
+let links = [
+
+    {
+      label: "Week1 notes",
+      url: "Week1/index.html"
+    },
+
+    {
+        label: "Week2 notes",
+        url: "week2/index.html"
+    }
+
+];
+
+for (let pairLabelLink of links){
+    
+    let classLiName = pairLabelLink["label"].replace(" ", "-");
+    let newLiElement = document.createElement("li");
+    newLiElement.setAttribute("class", classLiName);
+    taskList.appendChild(newLiElement);
+
+    let linkClassName = classLiName + "-link";
+    let newAElement = document.createElement("a");
+    newAElement.setAttribute("class", linkClassName);
+    newAElement.setAttribute("href", pairLabelLink["url"]);
+    newAElement.textContent = pairLabelLink["label"];
+
+    newLiElement.appendChild(newAElement);
+    /*console.log(pairLabelLink["url"]);*/
+};
